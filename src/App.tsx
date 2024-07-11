df803c9ea281d939e4d7dfce188a9259005e08cf
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-07-11 15:54:30 +0700

    Add Images Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import LOGO from './assets/IconNoBorder.svg';
+
 function App() {
   return (
-    <div className="size-full">
-      <h1>ARShip</h1>
+    <div className="min-h-screen w-full">
+      <div className="my-2 flex w-full">
+        <div className="">
+          <img 
+            src={LOGO} 
+            alt="ARShip Logo" 
+            className='size-8'
+          />
+        </div>
+      </div>
     </div>
   );
 }
