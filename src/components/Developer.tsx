028301fce3b49d32a88ca8b7899096fba8ad7b6f
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-07-13 13:05:58 +0700

    Edit Developer

--- src/components/Developer.tsx
+++ src/components/Developer.tsx
@@ -1,32 +1,26 @@
+import DEVICON from "../assets/Images/IconJael.svg"
+
 const Developer = () => {
   return (
     <section id="Developer" className="font-content text-gray-600">
       <div className="container mx-auto flex flex-col px-5 py-24">
         <div className="mx-auto lg:w-4/6">
-          <div className="h-64 overflow-hidden rounded-lg">
+          {/* <div className="h-64 overflow-hidden rounded-lg">
             <img alt="content" className="size-full object-cover object-center" src="https://dummyimage.com/1200x500"/>
-          </div>
+          </div> */}
           <div className="mt-10 flex flex-col sm:flex-row">
             <div className="text-center sm:w-1/3 sm:py-8 sm:pr-8">
               <div className="inline-flex size-20 items-center justify-center rounded-full bg-gray-200 text-gray-400">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="size-10" viewBox="0 0 24 24">
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </svg>
+                <img src={DEVICON} alt="Developer Icon" />
               </div>
               <div className="flex flex-col items-center justify-center text-center">
-                <h2 className="mt-4 font-title text-lg font-medium text-gray-900">Phoebe Caulfield</h2>
-                <div className="mb-4 mt-2 h-1 w-12 rounded bg-blue-500"></div>
-                <p className="text-base">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
+                <h2 className="mt-4 font-title text-lg font-medium text-gray-900">Jaelani.dev</h2>
+                <div className="mb-4 mt-2 h-1 w-12 rounded bg-yellow-500"></div>
+                <p className="text-base">Mainly work on Web Development, but decide to jump to game Development</p>
               </div>
             </div>
             <div className="mt-4 border-t border-gray-200 pt-4 text-center sm:mt-0 sm:w-2/3 sm:border-l sm:border-t-0 sm:py-8 sm:pl-8 sm:text-left">
-              <p className="mb-4 text-lg leading-relaxed">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
-              <a className="inline-flex items-center text-blue-500">Learn More
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="ml-2 size-4" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
+              <p className="mb-4 text-lg leading-relaxed">Our studio, a creative hub known for its exceptional web design and stunning photography, has recently expanded its horizons to embrace the exciting world of game development. With a team of talented designers and photographers, we've always been passionate about storytelling and visual artistry. Now, we're channeling that passion into creating immersive gaming experiences. Our aim is to combine our expertise in aesthetics and user experience to craft games that are not only visually captivating but also deeply engaging.</p>
             </div>
           </div>
         </div>
