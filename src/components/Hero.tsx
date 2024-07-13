cf041dc9aeb92a440a12dec58f6a0512dab510a6
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-07-13 13:19:22 +0700

    Edit Testimonials

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <img className="mb-10 w-5/6 rounded object-cover object-center md:w-3/6 lg:w-2/6" alt="hero" src={HERO}/>
 
 
-        <div className="mb-4 flex w-full flex-col items-center text-center md:w-2/3">
+        <div className="mb-16 flex w-full flex-col items-center text-center md:w-2/3">
           <h1 className=" mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">Mobile Educational History</h1>
           <p className="mb-8 leading-relaxed">Explore maritime history with ARShip, the app that brings iconic vessels and naval events to life through augmented reality. Perfect for history enthusiasts and educators, ARShip offers an engaging and interactive learning experience.</p>
           
--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -9,27 +9,28 @@ const Testimonial = () => {
               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="mb-4 block size-5 text-gray-400" viewBox="0 0 975.036 975.036">
                 <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
               </svg>
-              <p className="mb-6 leading-relaxed">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+              <p className="mb-6 leading-relaxed">ARShip has transformed the way I explore and understand maritime history. The augmented reality feature is incredibly immersive, allowing me to visualize historic ships right in my living room. It's a fantastic educational tool that's both engaging and informative.</p>
               <a className="inline-flex items-center">
-                <img alt="testimonial" src="https://dummyimage.com/106x106" className="size-12 shrink-0 rounded-full object-cover object-center"/>
+                <img alt="testimonial" src="https://ui-avatars.com/api/?name=Fadhillah%20AMP&background=F19601&color=fff" className="size-12 shrink-0 rounded-full object-cover object-center"/>
                 <span className="flex grow flex-col pl-4">
-                  <span className="font-title font-medium text-gray-900">Holden Caulfield</span>
-                  <span className="text-sm text-gray-500">UI DEVELOPER</span>
+                  <span className="font-title font-medium text-gray-900">Fadhillah AMP</span>
+                  <span className="text-sm text-gray-500">Game Developer</span>
                 </span>
               </a>
             </div>
           </div>
+
           <div className="w-full p-4 md:w-1/2">
             <div className="h-full rounded bg-gray-100 p-8">
               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="mb-4 block size-5 text-gray-400" viewBox="0 0 975.036 975.036">
                 <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
               </svg>
-              <p className="mb-6 leading-relaxed">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+              <p className="mb-6 leading-relaxed">As a game designer, I'm always on the lookout for innovative ways to learn and be inspired. ARShip's interactive learning approach and detailed 3D models of ships have not only educated me but also sparked new creative ideas for my own projects. It's a must-have app for anyone interested in history or design.</p>
               <a className="inline-flex items-center">
-                <img alt="testimonial" src="https://dummyimage.com/107x107" className="size-12 shrink-0 rounded-full object-cover object-center"/>
+                <img alt="testimonial" src="https://ui-avatars.com/api/?name=Tirta%20Wira&background=F19601&color=fff" className="size-12 shrink-0 rounded-full object-cover object-center"/>
                 <span className="flex grow flex-col pl-4">
-                  <span className="font-title font-medium text-gray-900">Alper Kamu</span>
-                  <span className="text-sm text-gray-500">DESIGNER</span>
+                  <span className="font-title font-medium text-gray-900">Tirta Wira</span>
+                  <span className="text-sm text-gray-500">Game Designer</span>
                 </span>
               </a>
             </div>
