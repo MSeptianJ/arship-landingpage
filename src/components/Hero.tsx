ebb9f6805a0c170fac1af5fae3011a85e0e55a6a
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-07-13 12:25:09 +0700

    Edit Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,20 @@
 import { PlayStoreIcon } from "../libs/Icons"
+import HERO from "../assets/Images/HeroImg.svg";
+import WAVEA from "../assets/Images/Vector-1.svg";
+import WAVEB from "../assets/Images/Vector-2.svg";
+import WAVEC from "../assets/Images/Vector-3.svg";
 
 const Hero = () => {
   return (
-    <section id="Home" className="font-content text-gray-600">
+    <section id="Home" className="font-content">
       <div className="container mx-auto flex flex-col items-center justify-center px-5 py-24">
-        <img className="mb-10 w-5/6 rounded object-cover object-center md:w-3/6 lg:w-2/6" alt="hero" src="https://dummyimage.com/720x600"/>
-        <div className="mb-16 flex w-full flex-col items-center text-center md:w-2/3">
-          <h1 className=" mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">Knausgaard typewriter readymade marfa</h1>
-          <p className="mb-8 leading-relaxed">Kickstarter biodiesel roof party wayfarers cold-pressed. Palo santo live-edge tumeric scenester copper mug flexitarian. Prism vice offal plaid everyday carry. Gluten-free chia VHS squid listicle artisan.</p>
-          <div className="flex w-full items-end justify-center">
-            <div className="mr-4 w-2/4 text-left md:w-full lg:w-full xl:w-1/2">
-              <label htmlFor="hero-field" className="text-sm leading-7 text-gray-600">Placeholder</label>
-              <input type="text" id="hero-field" name="hero-field" className="w-full rounded border border-gray-300 bg-gray-100 px-3 py-1 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-blue-200"/>
-            </div>
-            <button className="inline-flex rounded border-0 bg-blue-500 px-6 py-2 text-lg text-white hover:bg-blue-600 focus:outline-none">Button</button>
-          </div>
-          <p className="mb-8 mt-2 w-full text-sm text-gray-500">Neutra shabby chic ramps, viral fixie.</p>
+        <img className="mb-10 w-5/6 rounded object-cover object-center md:w-3/6 lg:w-2/6" alt="hero" src={HERO}/>
+
+
+        <div className="mb-4 flex w-full flex-col items-center text-center md:w-2/3">
+          <h1 className=" mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">Mobile Educational History</h1>
+          <p className="mb-8 leading-relaxed">Explore maritime history with ARShip, the app that brings iconic vessels and naval events to life through augmented reality. Perfect for history enthusiasts and educators, ARShip offers an engaging and interactive learning experience.</p>
+          
           <div className="flex">
             <button className="inline-flex items-center rounded-lg bg-gray-100 px-5 py-3 hover:bg-gray-200 focus:outline-none">
               <PlayStoreIcon />
@@ -24,6 +23,14 @@ const Hero = () => {
                 <span className="font-medium">Google Play</span>
               </span>
             </button>
+          </div>
+        </div>
+
+        <div className="w-screen">
+          <div className="relative h-60 w-full">
+            <img className="absolute bottom-0 left-0 max-h-60 w-full object-cover" src={WAVEC} alt="" />
+            <img className="absolute bottom-0 left-0 max-h-60 w-full object-cover" src={WAVEB} alt="" />
+            <img className="absolute bottom-0 left-0 max-h-60 w-full object-cover" src={WAVEA} alt="" />
           </div>
         </div>
       </div>
