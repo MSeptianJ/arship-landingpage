64a0b6c78c014e2e12600b05474ff896d3fa2acd
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-07-15 16:19:24 +0700

    Edit Icon

--- src/libs/Icons.tsx
+++ src/libs/Icons.tsx
@@ -2,19 +2,28 @@ import { FaGooglePlay } from "react-icons/fa";
 import { TbAugmentedReality } from "react-icons/tb";
 import { MdOutlineHistoryEdu } from "react-icons/md";
 import { FaHandsClapping } from "react-icons/fa6";
+import { MdMenu } from "react-icons/md";
 
-export const PlayStoreIcon = () => {
-  return <FaGooglePlay/>
+interface IconProps {
+  style?: string
 }
 
-export const ARIcon = () => {
-  return <TbAugmentedReality className=" size-full"  /> 
+export const PlayStoreIcon = ({style}:IconProps) => {
+  return <FaGooglePlay className={`${style}`}/>
 }
 
-export const HistoryIcon = () => {
-  return <MdOutlineHistoryEdu className=" size-full"  />
+export const ARIcon = ({style}:IconProps) => {
+  return <TbAugmentedReality className={`size-full ${style}`}  /> 
 }
 
-export const InteractiveIcon = () => {
-  return <FaHandsClapping className=" size-full" />
+export const HistoryIcon = ({style}:IconProps) => {
+  return <MdOutlineHistoryEdu className={`size-full ${style}`}  />
+}
+
+export const InteractiveIcon = ({style}:IconProps) => {
+  return <FaHandsClapping className={`size-full ${style}`} />
+}
+
+export const MenuIcon = ({style}:IconProps) => {
+  return <MdMenu className={`size-full ${style}`}/>
 }
