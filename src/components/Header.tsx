220b3e82ffbe76d5219a019016c1bda1fd8a7f07
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-07-16 13:44:33 +0700

    Edit Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,20 +17,20 @@ const Header = () => {
           <span className="ml-3 font-title text-xl">ARShip</span>
         </a>
 
-        <nav className="hidden items-center justify-center gap-4 text-base md:mx-auto md:flex">
-          <a className="w-full p-4 hover:bg-yellow-200 hover:text-gray-900" href='#Home'>Home</a>
-          <a className="w-full p-4 hover:bg-yellow-200 hover:text-gray-900" href='#Feature'>Features</a>
-          <a className="w-full p-4 hover:bg-yellow-200 hover:text-gray-900" href='#Developer'>Developer</a>
-          <a className="w-full p-4 hover:bg-yellow-200 hover:text-gray-900" href='#Testimonial'>Testimonial</a>
-          <a className="w-full p-4 hover:bg-yellow-200 hover:text-gray-900" href='#Contact'>Contacs</a>
+        <nav className="hidden items-center justify-center text-base md:mx-auto md:flex">
+          <a className="w-full p-4 transition-all duration-500 hover:bg-yellow-200" href='#Home'>Home</a>
+          <a className="w-full p-4 transition-all duration-500 hover:bg-yellow-200" href='#Feature'>Features</a>
+          <a className="w-full p-4 transition-all duration-500 hover:bg-yellow-200" href='#Developer'>Developer</a>
+          <a className="w-full p-4 transition-all duration-500 hover:bg-yellow-200" href='#Testimonial'>Testimonial</a>
+          <a className="w-full p-4 transition-all duration-500 hover:bg-yellow-200" href='#Contact'>Contacs</a>
         </nav>
 
-        <button onClick={handleClick} className='rounded border-0 bg-accent p-2 text-base transition-colors duration-300 focus:bg-accent/50 md:hidden'>
+        <button onClick={handleClick} className='rounded border-0 bg-accent p-2 text-base transition-colors duration-300 focus:bg-yellow-300 md:hidden'>
           <MenuIcon style={"text-2xl block"}/>
         </button>
       </div>
 
-      <div className={`relative m-auto w-full transition-all duration-300 md:hidden ${toggleNav ? "top-0" : "top-[-300px]"}`}>
+      <div className={`relative m-auto w-full transition-all duration-500 ease-in-out md:hidden ${toggleNav ? "top-0" : "top-[-300px]"}`}>
         <nav className="absolute left-0 top-0 flex w-full flex-col items-center justify-center bg-accent text-center">
           <a onClick={handleClick} className="w-full py-4 text-lg active:bg-accentdark" href='#Home'>Home</a>
           <a onClick={handleClick} className="w-full py-4 text-lg active:bg-accentdark" href='#Feature'>Features</a>
