bee2e695787178e29f91f167e14b74651bb7bd5a
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-07-17 09:56:56 +0700

    add separator

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,32 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Testimonial from './components/Testimonial';
+import WAVEA from "./assets/Images/Vector-1.svg";
+import WAVEB from "./assets/Images/Vector-2.svg";
+import WAVEC from "./assets/Images/Vector-3.svg";
 
 function App() {
   return (
     <div className="min-h-screen w-full">
       <Header />
       <Hero />
+
+      <div className="w-full">
+        <img className="size-full" src={WAVEC} alt="" />
+      </div>
+
       <Feature />
+
+      <div className="w-full bg-clr1">
+        <img className="size-full" src={WAVEB} alt="" />
+      </div>
+
       <Developer />
+
+      <div className="w-full bg-clr2">
+        <img className="size-full" src={WAVEA} alt="" />
+      </div>
+
       <Testimonial />
       <Contact />
       <Footer />
